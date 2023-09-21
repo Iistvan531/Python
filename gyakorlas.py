c9b8213acb2f3e284ce4b1dc62fae48023090ab9
Istvan <144987146+Iistvan531@users.noreply.github.com>
2023-09-21 08:22:54 +0200

    Add files via upload

---
+++ gyakorlas.py
@@ -0,0 +1,122 @@
+#1. f.: Kérjöünk be a felhasználotól két egész számot és vizsgálujk meg melyik a nagyobb vagy egyenlő.
+
+szam1 = int(input("add meg egy szamot"))
+szam2 = int(input("add meg egy szamot"))
+if  szam1 > szam2:
+    print("az első szam nagyobb mint a második")
+elif szam1 < szam2:
+    print("az első szam kissebb mint a  második")
+else:
+    print("a két szam egyenlő")
+
+
+#2. f.: Bekérunk 5 db egész számot megvizsgáljuk hogy pozitív vagy negatív és nagyobb mint 20 vagy kisseb.
+
+a = int(input("addj meg egy szamot"))
+b = int(input("addj meg egy szamot"))
+c = int(input("addj meg egy szamot"))
+d = int(input("addj meg egy szamot"))
+e = int(input("addj meg egy szamot"))
+szam = 0
+
+
+if a > 0:
+    print("pozitív")
+else:
+    print("negatív")
+if a > 20:
+    print("nagyobb mint 20")
+else:
+    print("kissebb mint 20")
+    
+    
+if b > 0:
+    print("pozitív")
+else:
+    print("negatív")
+if b > 20:
+    print("nagyobb mint 20")
+else:
+    print("kissebb mint 20")
+    
+    
+if c > 0:
+    print("pozitív")
+else:
+    print("negatív")
+if c > 20:
+    print("nagyobb mint 20")
+else:
+    print("kissebb mint 20")
+    
+    
+if d > 0:
+    print("pozitív")
+else:
+    print("negatív")
+if d > 20:
+    print("nagyobb mint 20")
+else:
+    print("kissebb mint 20")
+    
+    
+if e > 0:
+    print("pozitív")
+else:
+    print("negatív")
+if e > 20:
+    print("nagyobb mint 20")
+else:
+    print("kissebb mint 20")
+    
+if a > szam + 1:
+    print("szam pozitív")
+else:
+    print("szam negatív")
+
+if b > szam + 1:
+    print("szam pozitív")
+else:
+    print("szam negatív")
+    
+if c > szam + 1:
+    print("szam pozitív")
+else:
+    print("szam negatív")
+    
+    
+if d > szam + 1:
+    print("szam pozitív")
+else:
+    print("szam negatív")
+    
+if e > szam + 1:
+    print("szam pozitív")
+else:
+    print("szam negatív")
+    
+    
+
+#3. f.: számold meg hány olyan van az 5 közül hogy melyik nagyobb és potitív.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
